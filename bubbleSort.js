da27480e7f7cf158997eab1ca9e14251f166c247
Clara Swanson <claraswanson671@gmail.com>
2023-02-20 11:46:18 +0100

    Optimized bubble sort algorithm

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,11 +1,16 @@
-export default function bblSort(numbers) {
-    for (let i = 0; i < numbers.length; i++) {
-        let isSorted = true; // assumption that array is already sorted at the start of pass #i
-        for (let j = 0; j < (numbers.length); j++) {
+"use strict";
+exports.__esModule = true;
+function bblSort(numbers) {
+    var _a;
+    for (var i = 0; i < numbers.length - 1; i++) {
+        var isSorted = true; // assumption that array is already sorted at the start of pass #i
+        for (var j = 0; j < (numbers.length - 1 - i); j++) {
             if (numbers[j] > numbers[j + 1]) {
-                let temp = numbers[j];
-                numbers[j] = numbers[j + 1];
-                numbers[j + 1] = temp;
+                _a = [numbers[j + 1], numbers[j]], numbers[j] = _a[0], numbers[j + 1] = _a[1];
+                // ^ Array destructuring, same as the below 3 lines:
+                // let temp = numbers[j];
+                // numbers[j] = numbers[j+1];
+                // numbers[j+1] = temp;
                 isSorted = false; // if we end up swapping two items, then our previous assumption was wrong and we will make another pass of iterations       
             }
         }
@@ -15,3 +20,4 @@ export default function bblSort(numbers) {
     }
     return numbers;
 }
+exports["default"] = bblSort;
--- bubbleSort.ts
+++ bubbleSort.ts
@@ -1,11 +1,13 @@
 export default function bblSort(numbers: Array<number>): Array<number> {
-    for (let i=0; i<numbers.length; i++) {
+    for (let i=0; i<numbers.length-1; i++) {
         let isSorted = true; // assumption that array is already sorted at the start of pass #i
-        for (let j=0; j<(numbers.length); j++) {
+        for (let j=0; j<(numbers.length-1-i); j++) {
             if (numbers[j] > numbers[j+1]) {
-                let temp = numbers[j];
-                numbers[j] = numbers[j+1];
-                numbers[j+1] = temp;
+                [numbers[j], numbers[j+1]] = [numbers[j+1], numbers[j]];
+                // ^ Array destructuring, same as the below 3 lines:
+                // let temp = numbers[j];
+                // numbers[j] = numbers[j+1];
+                // numbers[j+1] = temp;
                 isSorted = false; // if we end up swapping two items, then our previous assumption was wrong and we will make another pass of iterations       
             }
         }
